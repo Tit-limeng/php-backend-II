29af6c40eb7377fb887eb798b5f3c62d1d05bc61
Tit limeng <titlymeng720@gmail.com>
2024-05-01 23:28:09 +0700

    testing

---
+++ classAttendance.php
@@ -0,0 +1,63 @@
+<?php
+
+class Attendance{
+    public $id ;
+    public $username;
+    public $gender;
+    public $password;
+    public $email;
+    public $start_date;
+    public $end_date;
+    public $date;
+    public $logIn ;
+
+    private function getAttendance(){
+        if ( session_id() == ""){
+            session_start();
+        }
+        $this -> id = $_SESSION["id"];
+        $this -> username = $_SESSION["username"];
+        $this -> gender = $_SESSION["gender"];
+        $this -> password = $_SESSION["password"];
+        $this -> email = $_SESSION["email"];
+        $this -> date = $_SESSION["date"];
+        $this -> logIn = $_SESSION["logIn"];
+    }
+
+    public function getSession(){
+        if ( session_id() == ""){
+            session_start();
+        }
+
+        $_SESSION["id"] = $this -> id;
+        $_SESSION["username"] = $this -> username;
+        $_SESSION["gender"] = $this -> gender;
+        $_SESSION["password"] = $this -> password ;
+        $_SESSION["email"] = $this -> email ;
+        $_SESSION["date"] = $this -> date;  
+        $_SESSION["logIn"] = $this -> logIn;
+    }
+
+    function __construct(){
+    //    if ( $_SESSION["logIn"] == true && isset($_SESSION["logIn"])) {
+        $this -> getAttendance();
+        $this -> logIn = $_SESSION["logIn"];
+    //    }
+    }
+    function isLogIn(){
+        return isset($_SESSION["email"]);
+    }
+    
+    function logOut(){
+        session_destroy(); 
+        header("location:logIn.php");
+    }
+}
+
+$attendace = new Attendance();
+
+if ( $attendace->isLogIn() ) {
+    header('location:home.php');
+}
+
+?>
---
+++ signIn.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/signIn.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Sign In</title>
+</head>
+<body>
+    <div class="crop">
+    <div class="box-signin">
+        <form action="home.php">
+        <h1>Login</h1>
+        <span>Username</span>
+        <div class="username-input">
+            <div class="username-icon">
+            <i class="fa-regular fa-user"></i>
+            </div>
+            <div class="username-label">
+                <input type="text" placeholder="type your username">
+            </div>
+        </div>
+        <span>Password</span>
+
+        <div class="password-input">
+            <div class="password-icon">
+            <i class="fa-solid fa-lock"></i>
+            </div>
+            <div class="password-label">
+                <input type="text" placeholder="type your password">
+            </div>
+        </div>
+        <div class="forgot">
+        <a href="">Forgot Password</a>
+    </div>
+
+    <div class="btn-login">
+        <button>Login</button>
+    </div>
+    <div class="sign-up">
+        <a href="">Or Sign Up</a>
+    </div>
+    </div>
+    </form>
+    </div>
+    
+</body>
+</html>
